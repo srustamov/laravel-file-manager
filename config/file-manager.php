4375ab3202666e0e9f6e6c2b343b054c7fab7748
Samir Rustamov <rustemovv96@gmail.com>
2020-06-02 14:11:40 +0400

    add config

---
+++ config/file-manager.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'paths' => [
+        'base' => base_path(),
+        'hidden' => [
+            base_path('vendor/'),
+            base_path('node_modules/')
+        ],
+        'only' => []
+    ],
+
+    // Laravel File Manager route configuration
+    'route' => [
+        'prefix' => '/file-manager',
+        'domain' => '',
+        'namespace' => 'Srustamov\\FileManager\\Controllers',
+        'as' => 'fileManager',
+        'middleware' => ['web']
+    ],
+];
